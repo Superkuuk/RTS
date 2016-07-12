fd044096d773f65aceb40a78af747e61b2577335
Rutger <groen@friesappeltje.nl>
2016-07-12 13:53:05 +0200

    Initial chat system
    added css and js for a chat system. And added socket support.

--- client.engine.js
+++ client.engine.js
@@ -1,13 +1,17 @@
+// Engine class for all the socket actions
 var socket = io();
 
+// Testfunction, make a draggable test object
 $(document).ready(function(){
 	$('<div id="block" style="position: absolute; height: 100px; width: 100px; background-color: red">draggable</div>').draggable({
 		drag: function( event, ui ) {
 			socket.emit('draggable move', ui.position);
 		}
-	}).appendTo('body'); 
+	}).appendTo('body');
 });
 
-socket.on('draggable move', function(position){
+
+// socket action for the test object
+socket.on('draggable move return', function(position){
 	$('#block').offset({ top: position.top, left: position.left});
 });
